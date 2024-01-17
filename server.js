b008df8836b8690e122bca84747a4bfeaedc9965
tetuanka <terhikamula@gmail.com>
2024-01-17 13:45:30 +0200

    deploy

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/health', (req, res) => {
+    res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+    res.send('1')
+})
